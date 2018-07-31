02a7c792381d4d1ffe722a51854f9e8cc8295689
Joseph Hicks <jthicks@gmail.com>
2018-07-30 23:38:58 -0500

    first crack

---
+++ src/main/resources/db/migration/V1.0.1__insert.sql
@@ -0,0 +1,3 @@
+insert into GITHUB_PROJECT (ORG_NAME, REPO_NAME) values ('spring-projects', 'spring-boot');
+insert into GITHUB_PROJECT (ORG_NAME, REPO_NAME) values ('spring-io', 'initializr');
+insert into GITHUB_PROJECT (ORG_NAME, REPO_NAME) values ('spring-io', 'saga');
